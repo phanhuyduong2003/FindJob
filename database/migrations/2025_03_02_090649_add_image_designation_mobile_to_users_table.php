0270916d73b97ec15d5912bec7de5473713d8d6a
phanhuyduong2003 <huyduongphan8@gmail.com>
2025-03-02 09:11:26 +0000

    [Update] - Enhance User model with additional fields and update .gitignore for profile pictures

--- database/migrations/2025_03_02_090649_add_image_designation_mobile_to_users_table.php
+++ database/migrations/2025_03_02_090649_add_image_designation_mobile_to_users_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-            $table->string('role');
+            $table->string('image')->nullable()->after('email');
+            $table->string('designation')->nullable()->after('image');
+            $table->string('mobile', 15)->nullable()->after('designation');
         });
     }
 
@@ -22,8 +24,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-            $table->dropColumn('role');
+            $table->dropColumn(['image', 'designation', 'mobile']);
         });
     }
 };
